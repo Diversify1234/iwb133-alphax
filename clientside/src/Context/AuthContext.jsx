728fa378f206eb496fc88f8b7629b2741da413fc
aaishahhamdha <ah20011024@gmail.com>
2024-10-22 10:45:46 +0530

    Auth local storage fixed

--- clientside/src/Context/AuthContext.jsx
+++ clientside/src/Context/AuthContext.jsx
@@ -1,19 +1,32 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-  const [token, setToken] = useState('');
+  const [user, setUser] = useState(() => {
+ 
+    const savedUser = localStorage.getItem('user');
+    return savedUser ? JSON.parse(savedUser) : null;
+  });
+  const [token, setToken] = useState(() => {
+    
+    return localStorage.getItem('token') || '';
+  });
 
   const login = (userData, token) => {
     setUser(userData);
     setToken(token);
+  
+    localStorage.setItem('user', JSON.stringify(userData));
+    localStorage.setItem('token', token);
   };
 
   const logout = () => {
     setUser(null);
     setToken('');
+   
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
   };
 
   return (
